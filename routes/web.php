ce3dd63c15251ac9b4b6c61a7ab8a03e095d2056
Ryan William O'Hara <ryan@ryanwohara.com>
2022-09-26 20:18:42 -0600

    Generate gender

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::get('/names', function () {
 
 Route::get('/name', function () {
     $species = ["human", "dwarf", "elf"][random_int(0, 2)];
+    $gender = ["male", "female", "non-binary"][random_int(0, 2)];
 
     $names = Name::all();
     $first_names = [];
@@ -39,23 +40,30 @@ Route::get('/name', function () {
         $name->attributes = $name->attributes()->get();
         $attributes = [];
         foreach ($name->attributes as $attribute) {
-            $attributes = array_merge($attributes, [$attribute->name => $attribute->value]);
+            $attributes[$attribute->name] = $attribute->value;
         }
         if ($attributes["species"] == $species) {
-            if ($attributes["name"] == "first") {
+            if ($attributes["name"] == "first" && $attributes["gender"] == $gender) {
                 array_push($first_names, $name->name);
             } elseif ($attributes["name"] == "last") {
                 array_push($last_names, $name->name);
             }
         }
     }
-    $first_name = $first_names[random_int(0, count($first_names) - 1)];
+
+    if (count($first_names)) {
+        $first_name = $first_names[random_int(0, count($first_names) - 1)];
+    } else {
+        $first_name = "Alex";
+    }
+
     $last_name = $last_names[random_int(0, count($last_names) - 1)];
-    $name = $first_name . ' ' . $last_name;
+    $name = $first_name . " " . $last_name;
 
     $npc = [
-        'name' => $name,
-        'species' => $species,
+        "name" => $name,
+        "species" => $species,
+        "gender" => $gender,
     ];
 
     return response()->json($npc);
